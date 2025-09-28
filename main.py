0c22d976def1706dbad6d01988b7be943edce669
Tharsan <tharsankartik2008@gmail.com>
2025-09-28 22:46:30 +0530

    Added add functionality

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # IMPORTS
 import csv
+import funcs as f
+from datetime import date
 
 # Possible Commands
 HELP = """
@@ -10,6 +12,7 @@ delete: deletes a task
 mark: marks a task as done
 unmark: unmarks a as undone
 get: get all the tasks
+exit: to exit the app
 """
 # Welcome to app message
 print()
@@ -20,3 +23,14 @@ print(HELP)
 
 while True:
     cmd = input(">>>")
+    if cmd.lower() == "add":
+        t_name = input("Enter task name: ")
+        t_status = False
+        t_due_date = int(input("Enter date: "))
+        t_due_month = int(input("Enter month: "))
+        t_due_year = int(input("Enter year: "))
+        due_date = date(t_due_year,t_due_month,t_due_date)
+        overdue = False
+        f.add([t_name,t_status,due_date,overdue])
+    elif cmd.lower() == "exit":
+        break
