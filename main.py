465c9aec8bfdb64172f6be57b532111ebb3593ed
Tharsan Karthikeyan <tharsankartik2008@gmail.com>
2025-10-01 19:41:11 +0530

    Structured and Extensively Commented the main.py file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# IMPORTS
+# REQUIRED IMPORTS
 import funcs
 from datetime import datetime
-# Possible Commands
+
+# USER HELP
 HELP = """
 Welcome to help: Here are all the commands you can use
 help: prints this message
@@ -13,16 +14,27 @@ get: get all the tasks
 delete tasklist: delete a tasklist
 exit: to exit the app
 """
-# Welcome to app message
+# WELCOME TO THE APP MESSAGE
 print()
 print("WELCOME TO TERMINAL TODOIST: An App for Student Productivity")
-# Print Help Message at the start of app
+
+# PRINTS HELP MESSAGE AT THE START OF THE PROGRAM
 print(HELP)
+
+# DATABASE UPDATION TO MATCH THE SYSDATE
 funcs.update_database()
 
-# Handling User Input
+# HANDLING USER INPUT
+# THIS IS A TERMINAL/COMMAND LINE BASED APP WITH USER COMMAND IT DOES NOT HAVE GUI(GRAPHICAL USER INTERFACE)
 while True:
+    # MAKING IT LOOK LIKE THE TERMINAL
     cmd = input(">>>")
+
+    #  THE HELP FUNCTIONALITY
+    if cmd.lower() == "help":
+        print(HELP)
+
+    # THE ADD FUNCTIONALITY
     if cmd.lower() == "add":
         t_lst = input("Enter Tasklist Name: ")
         t_name = input("Enter Task Name: ")
@@ -40,26 +52,37 @@ while True:
         lst = [t_lst,t_name,t_status,date,overdue]
         funcs.add(lst)
         print("Successfully Added")
+    
+    # THE DELETE FUNCTIONALITY
     elif cmd.lower() == "delete":
         task_name = input("Enter task name: ")
         task_list = input("Enter the name of the list: ")
         funcs.delete(task_list,task_name)
+    
+    # THE DELETE TASKLIST FUNCTIONALITY
     elif cmd.lower() == "delete tasklist":
         task_list = input("Enter task list name: ")
         funcs.delete_task_list(task_list)
+
+    # THE MARK FUNCTIONALITY
     elif cmd.lower() == "mark":
         task_name = input("Enter task name: ")
         task_list = input("Enter the name of the list: ")
         funcs.mark(task_name,task_list)
+    
+    # THE UNMARKS FUNCTIONALITY
     elif cmd.lower() == "unmark":
         task_name = input("Enter task name: ")
         task_list = input("Enter the name of the list: ")
         funcs.unmark(task_name,task_list)
+    
+    # THE GET ALL FUNCTIONALITY
     elif cmd.lower() == "get":
         task_list = input("Enter task list name or Click Enter to get all task list1: ")
         funcs.get(task_list)
     
-
-
+    # THE EXIT FUNCTIONALITY
     elif cmd.lower() == "exit":
         break
+
+# NOTE: THE REQUIRED FUNCTIONS USED IN THIS PROJECT ARE IN THE funcs.py FILE PLEASE REFER
